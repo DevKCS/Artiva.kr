3bd742e3bdb01823b5d6576b2dc94cc9bc4a9183
DevKCS <80586242+DevKCS@users.noreply.github.com>
2023-09-19 17:47:04 +0900

    Update GenerationTypes.ts

--- ts/lib/types/GenerationTypes.ts
+++ ts/lib/types/GenerationTypes.ts
@@ -1,57 +1,27 @@
-import { JobStatus, AspectRatio, ArtivaModel, Sampler } from "./GeneralTypes";
+import { JobStatus, AspectRatio, Sampler } from "./GeneralTypes";
 
 
 export interface CreateGenerationRequest {
-    model: ArtivaModel,
+    model: string,
     prompt: string,
-    negative_prompt?: string,
-    steps: number,
-    cfg_scale?: number,
-    clip_skip?: boolean,
+    negative?: string,
+    steps?: number,
     seed?: number,
     scheduler?: Sampler,
     karras?: boolean,
     aspect_ratio?: AspectRatio,
     title?: string,
-    desc?: string
+    description?: string
 }
 
 export interface ResponseParamType {
-    prompt: string,
-    negative_prompt: string,
-    seed: Number,
-    cfg: Number,
-    clip_skip: Boolean,
-    aspect_ratio: AspectRatio,
-    scheduler: Sampler,
-    karras: Boolean,
-    step: Number,
-    model: ArtivaModel
+    id: string,
+    url:string,
+    og_url:string,
+    time:number
 }
 
 export interface GenerationResponse {
-    /**
-     * The id of the job that has been created
-     */
-    id: string,
-
-    /**
-     * The current status of the job that has been created
-     */
     status: JobStatus,
-
-    /**
-     * A list of parameters that had been used t ocreate the job
-     */
-    params: ResponseParamType,
-
-    /**
-     * The URL of the generated image
-     */
-    imgUrl: string | undefined,
-
-    /**
-     * The ogURL of the generated image
-     */
-    ogUrl: string | undefined,
+    data: GenerationResponse
 }
