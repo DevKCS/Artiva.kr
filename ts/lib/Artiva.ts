7575d3817540ffe46732b6a3952ffff684811341
DevKCS <80586242+DevKCS@users.noreply.github.com>
2023-09-19 17:46:34 +0900

    Update Artiva.ts

--- ts/lib/Artiva.ts
+++ ts/lib/Artiva.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 import { CreateGenerationRequest, GenerationResponse } from "./types/GenerationTypes";
 import { UpscaleResponse, UpscaleType } from "./types/ImageUpscaleTypes";
+import { ImageToImageRequest } from "./types/ImageToImageTypes";
 
-const ARTIVA_URL = `https://artiva.kr`;
+const ARTIVA_URL = `https://api.artiva.kr`;
 
 /**
  * Them main artiva class, this class should be viewed as the instance of an API key
@@ -21,13 +22,32 @@ export class Artiva {
      */
     public generateText2Image(generationRequest: CreateGenerationRequest): Promise<GenerationResponse> {
         return new Promise(async (_r, _e) => {
-            const response = await axios.post(`${ARTIVA_URL}/api/img/t2i`, generationRequest, {
+            const response = await axios.post(`${ARTIVA_URL}/api/generate/txt2img`, generationRequest, {
                 headers: {
                     "artiva-api-key": this._apiKey
                 }
             })
-            if (response.status != 200) {
-                return _e(new Error(`Api responded with ${response.status} ${response.statusText}`))
+            if (response.status == 400) {
+                return _e(new Error(`Api responded with ${response.status} ${response.data.message}`))
+            }
+            const responseJSON = await response.data;
+            return _r(responseJSON as GenerationResponse);
+        })
+    }
+
+    /**
+     * Generate an image from a text prompt
+     * @param generationRequest The generation parameters for the request
+     */
+    public generateImage2Image(generationRequest: ImageToImageRequest): Promise<GenerationResponse> {
+        return new Promise(async (_r, _e) => {
+            const response = await axios.post(`${ARTIVA_URL}/api/generate/img2img`, generationRequest, {
+                headers: {
+                    "artiva-api-key": this._apiKey
+                }
+            })
+            if (response.status == 400) {
+                return _e(new Error(`Api responded with ${response.status} ${response.data.message}`))
             }
             const responseJSON = await response.data;
             return _r(responseJSON as GenerationResponse);
@@ -38,11 +58,10 @@ export class Artiva {
      * Upscale image from a id
      * @param id The generation id for the request
      */
-    public imageUpscale(id: String, type: UpscaleType): Promise<UpscaleResponse> {
+    public imageUpscale(id: String): Promise<UpscaleResponse> {
         return new Promise(async (_r, _e) => {
-            const response = await axios.post(`${ARTIVA_URL}/api/img/upscale`, {
-                id: id,
-                type: type
+            const response = await axios.post(`${ARTIVA_URL}/api/upscale`, {
+                id: id
             }, {
                 headers: {
                     "artiva-api-key": this._apiKey
