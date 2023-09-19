47e0062349efbfe567343d03ef22d6f63dd926a1
DevKCS <80586242+DevKCS@users.noreply.github.com>
2023-09-19 17:46:56 +0900

    Update ImageToImageTypes.ts

--- ts/lib/types/ImageToImageTypes.ts
+++ ts/lib/types/ImageToImageTypes.ts
@@ -1,5 +1,16 @@
+import { Sampler, AspectRatio } from "./GeneralTypes";
 import { CreateGenerationRequest } from "./GenerationTypes";
 
-export interface ImageToImageRequest extends CreateGenerationRequest {
-    image: string,
+export interface ImageToImageRequest {
+    img_url: string,
+    model: string,
+    prompt: string,
+    negative?: string,
+    steps?: number,
+    seed?: number,
+    scheduler?: Sampler,
+    aspect_ratio?: AspectRatio,
+    title?: string,
+    description?: string
 }
+
